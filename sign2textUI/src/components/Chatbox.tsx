97b11bf74f34e290e241a006eba43fc27593765f
jagrat7 <raojagrat@hotmail.com>
2023-06-08 21:27:22 -0400

    added working discussed UI and ml be

---
+++ sign2textUI/src/components/Chatbox.tsx
@@ -0,0 +1,154 @@
+import { Box, Button, Grid, List, ListItem, Paper, TextField, Typography } from "@mui/material";
+import { socket } from '../utils/socket';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react";
+interface ChatboxProps {
+    roomid: string | undefined
+    height: number
+}
+interface Emessaage {
+    topic: string
+    message: string
+}
+const Chatbox = ({ roomid, height }: ChatboxProps) => {
+
+
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    //   const [fooEvents, setFooEvents] = useState([]);
+    const [value, setValue] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [Emessaages, setEmessaages] = useState<string[] | []>([])
+    const [msgEvent, setMsgEvent] = useState([])
+    const [emitted, setemitted] = useState(false)
+
+    const submissionBox = useRef<HTMLDivElement>(null)
+    const CBheight = useRef<HTMLDivElement>(null)
+    const [msgHeight, setmsgHeight] = useState(0)
+    const messageElm = useRef(null)
+    const [paperHeight, setpaperHeight] = useState(0)
+    const [listHeight, setlistHeight] = useState(0)
+
+    function onSend(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        setIsLoading(true);
+        // console.log(textField.current.target.value)
+        if(value!=""){
+        socket.emit('message', { topic: roomid, message: value.toString() }, () => {
+            console.log("messSent", value.toString());
+
+            setIsLoading(false);
+        });}
+        setValue("")
+
+        // socket.timeout(5000).emit('chat message', {roomId : roomid, text : value.toString()}, () => {
+        //     setIsLoading(false);
+        //     setValue("")
+        // });
+
+    }
+
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+        if (paperHeight != undefined && listHeight != undefined && CBheight.current && submissionBox.current) {
+
+            setpaperHeight(CBheight?.current?.clientHeight)
+            console.log(paperHeight);
+
+            setlistHeight(submissionBox?.current?.clientHeight)
+            console.log(listHeight);
+
+
+            setmsgHeight(paperHeight - (listHeight * 2.3));
+        }
+
+
+        if (!emitted) {
+            socket.emit('subscribe', roomid, () => {
+                console.log("roomid sent");
+                setemitted(true)
+
+            });
+            setemitted(true)
+        }
+
+
+        socket.on("message", (Emessaage: Emessaage) => {
+            console.log("messReceived", Emessaage);
+            if (Emessaage.topic === roomid) {
+                setEmessaages(() => [...Emessaages, Emessaage.message]);}
+
+            
+            if (CBheight.current) {
+                CBheight.current.scrollTop = CBheight.current.scrollHeight - CBheight.current.clientHeight;
+            }
+        });
+        // socket.on("room#"+roomid, (Emessaage) => {
+        //     setEmessaages(() => [...Emessaages, Emessaage.text]);
+        // });
+        // console.log("mes",messages);
+        console.log("Emes", Emessaages);
+
+        // function onFooEvent(message: string) {
+        //   setFooEvents((prevMessages) => [...prevMessages, message]);
+        // }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        // socket.on('foo', onFooEvent);
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+            //   socket.off('foo', onFooEvent);
+        };
+    }, [Emessaages]);
+    return (
+        <div className="Chatbox">
+            <Paper ref={CBheight} elevation={8} sx={{ position: "relative", minHeight: height }} >
+                <Typography variant="h2" padding={"4%"} textAlign="center">Room #{roomid}</Typography>
+                <Box display="flex" flexDirection="column" justifyContent="space-between" >
+                    <Box sx={{ marginTop: "-3.8%", height: msgHeight, overflowY: "auto" ,backgroundColor:"#f7fbff" }}>
+                        <List >
+                            {Emessaages.map((emessage, index) => (
+                                <ListItem ref={messageElm} key={index}> <Typography>{emessage}</Typography> </ListItem>
+                            ))}
+
+                        </List>
+                    </Box>
+
+                    <Box ref={submissionBox} sx={{ position: "absolute", bottom: 0, width: "100%" }}>
+                        <form onSubmit={onSend}>
+                            <Grid direction="row"
+                                justifyContent="center"
+                                alignItems="stretch" container spacing={2}>
+
+                                <Grid item xs={9} sx={{}} >
+                                    <TextField value={value} onChange={e => setValue(e.target.value)}
+                                        id="outlined-basic" label="" sx={{ padding: "2%", width: "98%" }} variant="outlined" />
+                                </Grid>
+                                <Grid item xs={3} sx={{
+                                    display: "flex", justifyContent: "center",
+                                    flexWrap: "nowrap",
+                                    alignItems: "center",
+                                    paddingLeft: "0px"
+                                }}>
+                                    <Button type="submit" variant="contained" sx={{ width: "80%", height: "80%", }}>send</Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+
+                    </Box>
+                </Box>
+            </Paper>
+
+        </div>
+    );
+}
+
+export default Chatbox;
---
+++ sign2textUI/src/pages/Rooms.tsx
@@ -0,0 +1,151 @@
+import { Paper, Typography, Box, ListItemButton, ListItemText, List, ListItem, ListItemIcon, IconButton } from "@mui/material"
+import { Container } from '@mui/material';
+import SimpleDialog from "../components/SimpleDialog";
+import { ThemeProvider, } from '@mui/material/styles';
+import { StyledLink, themeTut } from "../utils/styles";
+import HubIcon from '@mui/icons-material/Hub';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useEffect, useState } from "react";
+
+interface Room {
+  roomId: number;
+  name: string;
+}
+
+export default function Rooms() {
+
+  const [rooms, setrooms] = useState<Room[]>([])
+  const [isloading, setisloading] = useState(true)
+
+  const [open, setOpen] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState<Room | null>(null);
+  const [roomID, setroomID] = useState<number>(0)
+
+  const handleClickOpen = (roomid: number) => {
+    setOpen(true);
+    setroomID(roomid)
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+
+  };
+  const handleDelete = (roomid: number) => {
+
+    const response = fetch(import.meta.env.VITE_CHATROOM_SERVICE_ROOMS, {
+      method: 'DELETE',
+      headers: { "Content-Type": "application/json" },
+
+      body: JSON.stringify({ "roomId": roomid })
+    })
+      .then(response =>{ response.text()
+      setrooms(rooms)
+    })
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  useEffect(() => {
+
+
+    const response = fetch(import.meta.env.VITE_CHATROOM_SERVICE_ROOMS, {
+
+    })
+      .then(response => {
+        // if (!response.ok) {
+        //   throw new Error("could not fetch")
+        // }
+        return response.json()
+      })
+      .then(data => {
+        console.log(rooms.toString());
+        console.log(data.toString());
+        
+        if(rooms.toString()!== data.toString()){
+          setrooms(data)
+          setisloading(false)}
+      })
+      .catch(error => {
+        setisloading(false)
+        console.error(error)
+      })
+
+
+  }, [rooms])
+
+
+  return (
+
+    <div className="Rooms">
+
+      <Container maxWidth='sm'>
+        <ThemeProvider theme={themeTut}>
+          <Box marginTop={"20%"} >
+            <Paper elevation={5} >
+              <Box >
+                <Typography paddingTop={"2%"} textAlign={"center"} variant="h3" component="h2">
+                  Rooms
+                </Typography>
+              </Box>
+              <List sx={{ display: "relative", marginX: "auto" }} >
+
+                {isloading &&
+
+                  <ListItem>
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <HubIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Loading rooms..." />
+                    </ListItemButton>
+                  </ListItem>
+                }
+
+                {!isloading && rooms.map((room) => {
+                  return (
+
+                    <ListItem key={room.roomId}>
+                      <ListItemButton onClick={() => handleClickOpen(room.roomId)}>
+                        <ListItemIcon>
+                          <HubIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={"Room " + room.name} />
+                      </ListItemButton>
+                      <IconButton onClick={() => handleDelete(room.roomId)} aria-label="delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItem>)
+
+                })
+                }
+                <StyledLink to={"/createRoom"}>
+
+                  <ListItem >
+                    <ListItemButton>
+                      <ListItemIcon>
+                        <AddIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Create a room" >
+                      </ListItemText>
+                    </ListItemButton>
+
+                  </ListItem>
+                </StyledLink>
+
+              </List>
+              <SimpleDialog
+                open={open}
+                onClose={() => setOpen(false)}
+                roomid={roomID}
+              />
+            </Paper>
+
+          </Box>
+        </ThemeProvider>
+      </Container>
+
+
+    </div >
+  )
+}
